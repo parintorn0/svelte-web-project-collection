2bfb7d52e6ce96c4134ac20f0299f57c4e12b5f2
Parintorn Thammasitboon <thammasitboonpt@gmail.com>
2024-04-27 08:50:35 +0700

    1

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		appDir: 'app',
-		adapter: adapter({
-			fallback: '200.html' // may differ from host to host
-		}),
-		paths: {
-            base: dev ? '' : process.env.BASE_PATH,
-        }
+		adapter: adapter()
 	}
 };
 
