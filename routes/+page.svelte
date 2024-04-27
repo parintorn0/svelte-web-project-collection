9d190393a1165bfda0fbabcc12578ca4bad73372
Parintorn Thammasitboon <thammasitboonpt@gmail.com>
2024-04-27 08:10:20 +0700

    Updates

---
+++ routes/+page.svelte
@@ -0,0 +1,81 @@
+<script>
+</script>
+
+<div class="home">
+	<span class="welcome">
+		Welcome to my SvelteJS web project lists
+	</span>
+	<div class="body">
+		<div class="container">
+			<a href="/image-viewer">
+				<img src="image_logo.png" alt="">
+				<span class="txt">Image Viewer</span>
+			</a>
+			<a href="/video-player">
+				<img src="video_logo.png" alt="">
+				<span class="txt">Video Player</span>
+			</a>
+		</div>
+	</div>
+</div>
+
+
+
+<style>
+	.home {
+		height: 100%;
+		width: 100%;
+		font-size: 50px;
+		font-family: Arial, Helvetica, sans-serif;
+		background: #f5f5f5;
+	}
+	.welcome {
+		position: absolute;
+		width: 100%;
+		padding-top: 5%;
+		color: rgba(17,17,17,1);
+		font-weight: 800;
+		text-align: center;
+	}
+	.body {
+		height: 100%;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-flow: column;
+	}
+
+
+	.container{
+		border-radius: 100px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 30%;
+		flex-flow: column;
+		background-color: rgba(17,17,17,0.4);
+	}
+
+	a{
+		display: flex;
+		align-items: center;
+		border-radius: 100px;
+		text-decoration: none;
+		color: #f5f5f5;
+		width: 100%;
+		margin: 5%, 0;
+	}
+
+	a:hover {
+		background-color: rgba(17,17,17,0.1);
+	}
+
+	.txt {
+		font-weight: 600;
+		color: #f5f5f5;
+	}
+	img {
+		height: 200px;
+	}
+</style>
---
+++ routes/image-viewer/+page.svelte
@@ -0,0 +1,83 @@
+<script>
+	import Dragdrop from "./Dragdrop.svelte";
+</script>
+
+<div class="image-viewer">
+	<div id='guide'>
+        <div id='key-guide'>Keys Function</div>
+		<div class=container>
+			<div class="grid-display">
+				<span class="one">a</span>
+				<span class="two">:</span>
+				<span class="three">See fit screen</span>
+			</div>
+			<div class="grid-display">
+				<span class="one">s</span>
+				<span class="two">:</span>
+				<span class="three">See fit height</span>
+			</div>
+			<div class="grid-display">
+				<span class="one">d</span>
+				<span class="two">:</span>
+				<span class="three">See fit width</span>
+			</div>
+			<div class="grid-display">
+				<span class="one">f</span>
+				<span class="two">:</span>
+				<span class="three">See original</span>
+			</div>
+		</div>
+      </div>
+	<Dragdrop />
+</div>
+
+<style>
+	.image-viewer {
+		display: flex;
+		height: 100%;
+		width: 100%;
+		background: #f5f5f5;
+		justify-content: center;
+	}
+	#guide {
+		top: -60px;
+		position: absolute;
+		z-index: 1;
+		padding: 20px;
+		padding-top: 70px;
+		background-color: rgba(0, 0, 0, 0.5);
+		color: rgb(240, 240, 240);
+		border-radius: 50px;
+		font-weight: 600;
+		width: 300px;
+		display: flex;
+		flex-direction: column;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+
+	#key-guide{
+		text-align: center;
+		margin-bottom: 40px;
+		font-size: 25px;
+		font-weight: 800;
+	}
+
+	.grid-display {
+		font-size: 20px;
+		display: grid;
+		grid-template-columns: 20px 20px 160px;
+	}
+	.one{
+		grid-column-start: 1;
+		grid-column-end: 2;
+	}
+	.two{
+		grid-column-start: 2;
+		grid-column-end: 3;
+	}
+	.three{
+		grid-column-start: 3;
+		grid-column-end: 4;
+	}
+
+</style>
