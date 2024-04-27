8ff6194d5481aa5ff1925aec88adeca3ed987526
Parintorn Thammasitboon <thammasitboonpt@gmail.com>
2024-04-27 07:31:19 +0700

    Set github Page

---
+++ src/routes/image-viewer/Dragdrop.svelte
@@ -0,0 +1,203 @@
+<script>
+    let hover = false;
+    let fileList=[];
+    let haveImg = false;
+    let currentImg='';
+    let currentIndex=0;
+    let isRight=true;
+    function handleDrop(e){
+        const data=e.dataTransfer;
+        const files = data.files;
+        const fileArray=[...files];
+        for(let i=0;i<fileArray.length;i++){
+            fileList = [...fileList,{'url':URL.createObjectURL(fileArray[i]), 'type':fileArray[i].type}];
+        }
+        if(!haveImg){
+            currentImg=fileList[0].url;
+            currentIndex=0;
+            haveImg=true;
+        }
+    }
+    function remove(index){
+        if(fileList[index].url===currentImg){
+            currentImg=fileList[0].url;
+            currentIndex=0;
+        }
+        fileList=fileList.filter((item)=>item!=fileList[index]);
+        if(fileList.length===0){
+            currentImg='';
+            haveImg=false;
+        }
+    }
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="image-viewer">
+	<section class="droparea {hover ? 'hover':''}"
+    on:dragenter|preventDefault={()=>hover = true}
+    on:dragover|preventDefault={()=>hover = true}
+    on:dragleave|preventDefault={()=>hover = false}
+    on:drop|preventDefault={(e)=>{
+        hover = false;
+        handleDrop(e);
+        }}
+    >
+    {#if hover}
+        <p class="drop-txt">Drop</p>
+        <p class="drop-txt">Now</p>
+    {:else}
+        <p class="drop-txt">Drop</p>
+        <p class="drop-txt">your</p>
+        <p class="drop-txt">Image</p>
+        <p class="drop-txt">here</p>
+    {/if}
+    </section>
+    <div class="img-nav">
+        {#key fileList}
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            {#each fileList as file, index}
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <img class="img-list"
+                class:select={index===currentIndex} src={file.url} alt="" on:click={()=>{currentImg=file.url; currentIndex=index}}>
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <span class="remove" on:click={()=>remove(index)}>&times</span>
+            {/each}
+        {/key}
+    </div>
+    <img class="current" src={currentImg} alt="">
+</div>
+
+<style>
+	.image-viewer {
+		height: 100%;
+		width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+		background: #f5f5f5;
+	}
+
+    img.current {
+        max-height: 100%;
+        max-width: 100%;
+    }
+
+    .droparea {
+        position: absolute;
+        right: 0;
+        top: 0;
+        margin: 1%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 10%;
+        height: 70%;
+        border: 4px dashed grey;
+        border-radius: 15px;
+    }
+
+    .droparea.hover {
+        background-color: rgba(17,17,17,0.5);
+        border: 4px solid rgba(17,17,17,1);
+    }
+
+    .drop-txt{
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: x-large;
+        font-weight: 800;
+        margin: 1rem;
+    }
+
+    .img-nav {
+        bottom: 0;
+        position: absolute;
+        height: 25%;
+        width: 100%;
+        display: flex;
+        background-color: rgba(17,17,17,0.5);
+        overflow-x: scroll;
+    }
+    
+    .img-list {
+        margin: 30px;
+        min-height: 150px;
+        min-width: 150px;
+        object-fit: contain;
+        border: 3px solid rgba(17,17,17,1);
+        border-radius: 35px;
+        background-color: #f5f5f5;
+    }
+
+    .img-list.select{
+        border: 3px solid green;
+    }
+
+    .remove{
+        color: #f5f5f5;
+        width: 20px;
+        font-size: 50px;
+        margin-left: -20px;
+        margin-top: 15px;
+        cursor: default;
+    }
+    .remove:hover{
+        font-weight: 900;
+    }
+    ::-webkit-scrollbar {
+        display: block;
+        height: 10px;
+    }
+    ::-webkit-scrollbar-thumb {
+        border-radius: 100px;
+        background-color: rgb(51, 51, 51);
+    }
+</style>
+
+<svelte:window on:keydown|preventDefault={(e)=>{
+    console.log(e.code);
+    function goLeft() {
+        if(currentIndex>0){
+            currentIndex--;
+            currentImg=fileList[currentIndex].url;
+        }
+        if(currentIndex<=0){
+            isRight=true;
+        }
+        else{
+            isRight=false;
+        }
+    }
+    function goRight() {
+        if(currentIndex<fileList.length-1){
+            currentIndex++;
+            currentImg=fileList[currentIndex].url;
+        }
+        if(currentIndex>=fileList.length-1){
+            isRight=false;
+        }
+        else{
+            isRight=true;
+        }
+    }
+    if(e.code==='ArrowLeft' || e.code==='ArrowUp'){
+        goLeft();
+    }
+    if(e.code==='ArrowRight' || e.code==='ArrowDown'){
+        goRight();
+    }
+    if(e.code==='Space'){
+        if(isRight){
+            goRight();
+        }
+        else{
+            goLeft();
+        }
+    }
+}}
+/>
+
+
---
+++ src/routes/image-viewer/VideoList.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let imgList;
+</script>
+
+<div class="container">
+    {@html imgList}
+</div>
+
+<style>
+    .container {
+        bottom: 0;
+        position: absolute;
+        height: 20%;
+        width: 100%;
+        display: flex;
+        background-color: aquamarine;
+    }
+</style>
