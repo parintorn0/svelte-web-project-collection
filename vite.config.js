5b096c42539c0cc51ba6553e3c4dd7d3f34c1fd2
Parintorn Thammasitboon <thammasitboonpt@gmail.com>
2024-04-27 08:02:51 +0700

    1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: 'svelte-web-project-collection',
+	base: '/svelte-web-project-collection/',
 	plugins: [sveltekit()]
 });
